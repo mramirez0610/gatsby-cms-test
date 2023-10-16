a2f4d3aa9ab7b2099307f75a8cea93f613e5df2d
marco ramirez <marco@marcos-Air.lan>
2023-10-16 19:10:57 -0400

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,10 @@
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  siteMetadata: {
+    title: `gatsby-test`,
+    siteUrl: `https://www.yourdomain.tld`
+  },
+  plugins: ["gatsby-plugin-netlify-cms", "gatsby-plugin-sass"]
+};
